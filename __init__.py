e4fe50d22c5a28cb3b0864e0bfecd79425136375
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-06-26 18:00:28 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,21 @@
+from .nodes import LoadPosterCraftPrompt, LoadPipeline, LoadCustomTransformer, LoadQwenModel, PosterCraft, SavePosterCraft
 
+NODE_CLASS_MAPPINGS = {
+    "LoadPosterCraftPrompt": LoadPosterCraftPrompt,
+    "LoadPipeline": LoadPipeline,
+    "LoadCustomTransformer": LoadCustomTransformer,
+    "LoadQwenModel": LoadQwenModel,
+    "PosterCraft": PosterCraft,
+    "SavePosterCraft": SavePosterCraft,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadPosterCraftPrompt": "Load PosterCraft Prompt",
+    "LoadPipeline": "Load Pipeline",
+    "LoadCustomTransformer": "Load Custom Transformer",
+    "LoadQwenModel": "Load Qwen Model",
+    "PosterCraft": "PosterCraft",
+    "SavePosterCraft": "Save PosterCraft",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
